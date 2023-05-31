30eb526d3efc5fae07b7252f1f69418ce5a2f568
mk020846 <132980253+mk020846@users.noreply.github.com>
2023-05-31 12:03:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import discord
+from discord.ext import commands
+from config import config
 
+intents = discord.Intents.defaults()
+intents.members = True
+
+bot = commands.Bot(command_prefix=config.PREFIX, intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"logged in as {bot.user.name}")
+
+if __name__ = '__main__':
+    for extension in config.EXTENSIONS:
+        bot.load_extension(extension)
+    bot.run(config.TOKEN)
