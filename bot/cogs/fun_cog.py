cca764d25b5acc2ce6f4ce044bf81245e55498ec
mk020846 <132980253+mk020846@users.noreply.github.com>
2023-05-31 12:27:27 +0530

    Update fun_cog.py

--- bot/cogs/fun_cog.py
+++ bot/cogs/fun_cog.py
@@ -1,1 +1,23 @@
+import asyncpraw
+import discord
+from discord.ext import commands
+from config import secrets
+
+class FunCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.reddit = asyncpraw.Reddit(client_id=secrets.REDDIT_CLIENT_ID,
+                                       client_secret=secrets.REDDIT_CLIENT_SECRET,
+                                       user_agent=secrets.REDDIT_USER_AGENT)
+
+    @commands.command()
+    async def memes(self, ctx):
+        subreddit = await self.reddit.subreddit("memes")
+        post = await subreddit.random()
+        embed = discord.Embed(title=post.title, color=discord.Color.random())
+        embed.set_image(url=post.url)
+        await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(MemesCog(bot))
 
