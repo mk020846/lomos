ae9e77ecf5082c0e8a4965a7008f9a51718dd13e
mk020846 <132980253+mk020846@users.noreply.github.com>
2023-05-31 12:06:54 +0530

    Create requirements.py

---
+++ requirements.py
@@ -0,0 +1,6 @@
+# a simple file to just check are all required packages installed?
+from os import system
+try:
+  system("python3 -m pip install discod python-dotenv")
+ except:
+  print("Packages either installed or any error occoured")
